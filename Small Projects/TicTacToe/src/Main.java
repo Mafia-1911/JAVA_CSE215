dd2ba30aa3f07414de90bacf818f75e2e73f2e5c
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-26 23:47:47 +0600

    Update Main.java

--- Small Projects/TicTacToe/src/Main.java
+++ Small Projects/TicTacToe/src/Main.java
@@ -29,8 +29,12 @@ class Board{
     boolean haveWon(){
         //check rows
         for (int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+            }
 
         }
+        return true;
     }
 
 
