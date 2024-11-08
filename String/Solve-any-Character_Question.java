01656247766706e98801afab6b5a67279ac496be
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-11-08 23:31:03 +0600

    Add files via upload

---
+++ String/Solve-any-Character_Question.java
@@ -0,0 +1,25 @@
+class Main {
+    
+}
+//Getting the ascii equivalent of a charecter --Typecast 
+int getAscii(char xyz){
+    return (int) xyz;
+}
+//Q. Convert uppercase to lowercase 
+//   - Add 32 and vice versa 
+//Q. What are Charecter.isSpace()
+// - ' ' or \n \t are all space 
+//Q. Convert uppercase to lower case and vice versa 
+// - Character.toUpper(input) || if(input>='A' && input<='Z')
+// Most of the Charecter Methods will have the parameter as char , All 
+// charecter methods are static methods so you need to write the method name along with the class name 
+// ie- Character.methodName(YourInput) 
+
+class Charecter_Operations{
+    main(){
+//Charecter Object
+        Character ch=new Charecter('c');
+        Character ch2='c';//Autobox feature 
+         
+    }
+}
